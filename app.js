d2fde5fec69c0df5112558f4d372ec281c6fc05f
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-09-15 13:31:16 +0200

    Initial setup

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require('express'),
+    bodyParser = require("body-parser"),
+    mongoose = require("mongoose"),
+    app = express();
+
+mongoose.connect("mongodb://127.0.0.1:27017/Blog_App", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+app.set("view engine", "ejs");
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+let Blog = mongoose.model("Blog", blogSchema);
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+Blog.create({
+    title: "Test Blog",
+    image: "https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80",
+    body: "Hello this is a blog post!"
+})
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+})
+
+app.listen(3000, () => {
+    console.log("Server listening on 3000");
+})
